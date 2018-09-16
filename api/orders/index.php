7573aa0b5a27e97122bfb722bcbac33afcbd025f
cavid24 <Bitbucket9099xy>
2018-09-17 02:57:28 +0400

    Added install.php file

--- api/orders/index.php
+++ api/orders/index.php
@@ -9,13 +9,18 @@
 include_once '../../autoload.php';
 header("Content-Type: application/json; charset=UTF-8");
 
+// Get method
 $method = isset($_GET['method']) ? $_GET['method'] : '';
+
+// Create new Orders instance
 $orders = new \Classes\Orders();
+
 switch($method) {
     /**
      * Take (update) the order
      */
     case 'take':
+        // Allow only PUT method
         header("Access-Control-Allow-Methods: PUT");
         $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
         $result = $orders->take($id);
@@ -27,6 +32,7 @@ switch($method) {
      * Get list of orders
      */
     case 'list':
+        // Allow only GET method
         header("Access-Control-Allow-Methods: GET");
 
         $results = $orders->records();
@@ -38,7 +44,7 @@ switch($method) {
      * Create new order
      */
     default:
-
+        // Allow only POST method
         header("Access-Control-Allow-Methods: POST");
         header("Access-Control-Max-Age: 3600");
         header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
