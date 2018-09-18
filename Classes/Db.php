a01a0aded3e1bea32f3c6c24490c4e444fdad3c4
cavid24 <Bitbucket9099xy>
2018-09-19 00:37:09 +0400

    composer.lock added

--- Classes/Db.php
+++ Classes/Db.php
@@ -16,7 +16,7 @@ class Db
 
     private $_connection;
     private static $_instance; //The single instance
-    private $_host = 'localhost';
+    private $_host = 'db';
     private $_username = 'root';
     private $_password = '';
     private $_database = 'apitest';
