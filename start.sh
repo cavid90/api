436d02e082cbd28ad2c904ada3a12d33316270af
cavid24 <Bitbucket9099xy>
2018-09-20 00:47:23 +0400

    Editing start.sh file

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 # Check of git package and install if does not exist
 dpkg -l | grep -qw git || apt-get -y install git
-
-# Clone repository for github to home/javid-api
-git clone https://github.com/cavid90/api.git /home/javid-api
+sudo mkdir /home/${USER}/javid-api
+sudo chmod -R 777 /home/${USER}/javid-api
+# Clone repository for github to /home/${USER}/javid-api
+git clone https://github.com/cavid90/api.git /home/${USER}/javid-api
 
 # go to the directory
-cd /home/javid-api
+cd /home/${USER}/javid-api
 
 # Run docker compose for http://localhost:8080
 docker-compose up -d
