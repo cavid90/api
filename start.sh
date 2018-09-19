c4d7e028a1b5eae2c5a1089f7b60d5aca3e4a911
cavid24 <Bitbucket9099xy>
2018-09-19 16:37:58 +0400

    composer.lock added

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Check of git package and install if does not exist
+dpkg -l | grep -qw git || apt-get -y install git
+
+# Clone repository for github to home/javid-api
+git clone https://github.com/cavid90/api.git /home/javid-api
+
+# go to the directory
+cd /home/javid-api
+
+# Run docker compose for http://localhost:8080
 docker-compose up -d
-docker-compose ps
